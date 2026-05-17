bf2ccc9d0429efeee86c9023b701357791c3ba04
galaxie667petasse-oss <galaxie667petasse@gmail.com>
2026-05-18 00:48:11 +0200

    Switch stable entrypoint to Oracle Bot V4.5

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Keep Railway Start Command fixed to: python main.py
 # To upgrade versions later, only change CURRENT_MODULE below.
 
-CURRENT_MODULE = "oracle_bot_v44"
+CURRENT_MODULE = "oracle_bot_v45"
 
 module = __import__(CURRENT_MODULE)
 module.main()
